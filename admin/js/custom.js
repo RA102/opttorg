2ae090dbf5bb54ecdd95884dcac264e3d354cdbf
RA <sanmarketopttorg00@mail.ru>
2021-02-12 18:00:49 +0600

    !

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -2,6 +2,10 @@ $(document).ready(function(event) {
     $('tr[name*="addPartItem"]').on('click', function(event) {
         const addRow = $(this).prev();
 
+        let cloneRow = addRow.clone();
+
+        cloneRow.find('input').val('');
+
         $(this).before(addRow.clone());
 
     })
